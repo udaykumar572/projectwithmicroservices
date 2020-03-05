22366d118457d7abdcbf7eb7ac48312f99402317
udaykumar572 <60086619+udaykumar572@users.noreply.github.com>
2020-03-05 18:10:32 +0530

    Add files via upload

---
+++ EurekaServer/src/test/java/com/cts/project/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.project.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
